babeedca785caf6d79801db99da3fae71f1203ae
Jorge_Enebral <202213346@alu.comillas.edu>
2025-04-05 14:16:01 +0200

    nueva bbdd

--- solvifyServer/settings.py
+++ solvifyServer/settings.py
@@ -30,9 +30,8 @@ SECRET_KEY = 'django-insecure-*mxnxlsl5+ru(bpfi%=)^v3&u4x8qfi44l+(gn3q2ch)u(-4h^
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", 
-                 "@dpg-cvnrr1k9c44c73ahrdvg-a.oregon-postgres.render.com", 
+                 "@dpg-cvohpqh5pdvs739utoj0-a.oregon-postgres.render.com", 
                  "solvifyserver.onrender.com"]
-
 
 # Application definition
 
