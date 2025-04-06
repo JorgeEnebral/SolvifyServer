160c9798bd2a57bde00b9f94e6c02ca8c50526b6
Jorge_Enebral <202213346@alu.comillas.edu>
2025-04-06 10:32:03 +0200

    settings 3

--- solvifyServer/settings.py
+++ solvifyServer/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'solvifyServer.urls'
@@ -151,8 +151,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1",
-    "http://localhost:3000",  # Agrega esta línea
-    "https://solvifyserver.onrender.com",
+    "http://localhost:3000",
+    "https://solvify-das.vercel.app/",
 ]
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
